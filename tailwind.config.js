d01b10cfc7619a405b59837dea4f632937c550da
Marko Maric <marko.maric@hotelplan.com>
2024-04-12 09:22:48 +0200

    Removed scaffolding code. Added tailwindcss with a basic configuration of colors.

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        bgr: {
+          DEFAULT: "#011627",
+          lighter: "#02223C",
+          darker: "#010B14",
+        },
+        txt: {
+          DEFAULT: "#FDFFFC",
+          lighter: "#FFFFFF",
+          darker: "#FDFFFC",
+        },
+        pri: {
+          DEFAULT: "#CE2D4F",
+          hover: "#C92C4E",
+          active: "#B82848",
+          contrast: "#FDFFFC",
+        },
+        sec: {
+          DEFAULT: "#4281A4",
+          hover: "#407FA0",
+          active: "#3A7392",
+          contrast: "#FDFFFC",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
