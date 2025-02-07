5d8cde54c533926121977bf845c5ae9c3e12b9d2
Marko Maric <marko.maric@hotelplan.com>
2025-02-07 16:58:39 +0100

    Added the new components to the index page so they can easily be accessed.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+const components = [
+  { name: "Buttons", path: "/components/buttons" },
+  { name: "Checkboxes", path: "/components/checkboxes" },
+  { name: "Radiobuttons", path: "/components/radiobuttons" },
+];
+</script>
+
 <template>
   <div>
     <h1>Home</h1>
 
-    <NuxtLink to="/components/buttons" class="text-sm text-blue-500 hover:text-blue-600 hover:underline">Buttons</NuxtLink>
+    <section class="flex flex-col gap-y-1">
+      <NuxtLink v-for="component in components" :to="component.path" class="text-sm text-blue-500 hover:text-blue-600 hover:underline">{{ component.name }}</NuxtLink>
+    </section>
   </div>
 </template>
